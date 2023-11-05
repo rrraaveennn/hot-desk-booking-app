b45c77e06b5d2fe5579389535d8e2074c24ca261
Raven <raydree13@gmail.com>
2023-11-05 23:03:52 +0800

    Updated Auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,8 @@
 import Image from "next/image";
 import { getSession } from "./api/auth/[...nextauth]/route";
-import { Resend } from "resend";
 
 export default async function Home() {
   const session = await getSession();
 
-  const resend = new Resend(process.env.RESEND_API_KEY);
-
-  const data = await resend.emails.send({
-    from: process.env.RESEND_EMAIL!,
-    to: [
-      "johnraybendelarama@student.laverdad.edu.ph",
-      "delaramajohnrayben@gmail.com",
-    ],
-    subject: "Welcome",
-    html: "<p>Hello World!<p>",
-  });
-
-  return <div>Hello {JSON.stringify(data)}!</div>;
+  return <div>Hello {session?.user.firstName}!</div>;
 }

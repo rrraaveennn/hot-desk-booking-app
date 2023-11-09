602d398c2e695d1bee3415732dcf1040582cd32b
Raven <raydree13@gmail.com>
2023-11-09 18:03:48 +0800

    Updated Terms

--- components/layouts/Footer.tsx
+++ components/layouts/Footer.tsx
@@ -16,14 +16,14 @@ function Footer() {
       <div className="flex flex-col justify-around sm:items-center h-full gap-5 sm:flex-row">
         <Link
           className="text-xs font-medium text-slate-600 transition-colors duration-75 ease-in hover:text-slate-400"
-          href="/privacy-policy"
+          href="https://www.termsfeed.com/live/692e4cf6-fab6-4248-b2d1-f70df161dd08"
           target="_"
         >
           Privacy Policy
         </Link>
         <Link
           className="text-xs font-medium text-slate-600 transition-colors duration-75 ease-in hover:text-slate-400"
-          href="/terms-and-conditions"
+          href="https://www.termsandconditionsgenerator.com/live.php?token=PdpUwBeiWejWjoz8FkuTNxFBvoQkiTGM"
           target="_"
         >
           Terms and Conditions

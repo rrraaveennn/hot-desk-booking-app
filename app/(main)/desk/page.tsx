1534cd4a52a7777e26eb77ab3b5c746b2a7e7383
Raven <raydree13@gmail.com>
2023-11-14 00:26:48 +0800

    Desk Map Initial Setup

--- app/(main)/desk/page.tsx
+++ app/(main)/desk/page.tsx
@@ -1,19 +1,12 @@
 "use client";
+
 import { DateTimePicker } from "@/components/DateTimePicker";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
-import axios from "axios";
 import z from "zod";
+
 import { getAvailableDesks } from "@/actions/actions";
 
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { toast } from "@/components/ui/use-toast";
 import { useState } from "react";
@@ -29,6 +22,7 @@ function Desk() {
   const form = useForm<z.infer<typeof FormSchema>>({
     resolver: zodResolver(FormSchema),
   });
+
   const [Bookings, setBookings] = useState<string[]>([]);
 
   async function onSubmit(data: z.infer<typeof FormSchema>) {
@@ -38,26 +32,17 @@ function Desk() {
       description: <p>{JSON.stringify(result)}</p>,
     });
   }
+
   return (
     <div className="p-3 sm:pt-10 sm:pl-10 flex flex-col space-y-5">
-      <Card className="">
-        <CardHeader className="">
-          <CardTitle>Desk map</CardTitle>
-          <CardDescription className="pb-3">
-            Select your desk now or whenever you want!
-          </CardDescription>
-          <Separator orientation="horizontal" />
-        </CardHeader>
-        <CardContent>
-          <DateTimePicker
-            label="Booking date"
-            description="See available desks."
-            form={form}
-            onSubmit={onSubmit}
-          />
-        </CardContent>
-      </Card>
-
+      <h1 className="text-xl font-bold font-sans">Desk map</h1>
+      <DateTimePicker
+        label="Booking date"
+        description="See available desks."
+        form={form}
+        onSubmit={onSubmit}
+      />
+      <Separator orientation="horizontal" />
       <DeskMap bookings={Bookings} />
     </div>
   );
